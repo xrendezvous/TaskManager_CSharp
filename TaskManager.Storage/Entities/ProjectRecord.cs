f57700aea7d569ba70177974211438cec2a8ee96
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2026-02-09 21:52:06 +0200

    changed some setters to private

--- TaskManager.Storage/Entities/ProjectRecord.cs
+++ TaskManager.Storage/Entities/ProjectRecord.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using TaskManager.Storage.Enums;
 
-
 namespace TaskManager.Storage.Entities
 {
     public class ProjectRecord
     {
         public int Id { get; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public TypeOfProject Type { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public TypeOfProject Type { get; private set; }
 
         public ProjectRecord(int id, string name, string desc, TypeOfProject type) 
         {
