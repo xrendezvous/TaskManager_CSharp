c4cfdcbcb90b5346832e189c93bacec24630b723
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2026-02-09 22:08:10 +0200

    added TaskView

--- TaskManager.Properties/TaskView.cs
+++ TaskManager.Properties/TaskView.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskManager.Storage.Enums;
 
-namespace TaskManager.Properties
+namespace TaskManager.Views
 {
-    internal class TaskView
+    public class TaskView
     {
+        public int Id { get; }
+        public int ProjectId { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public Priority Priority { get; }
+        public DateTime DueDate { get; }
+        public bool IsFinished { get; }
+        public bool IsOverdue => !IsFinished && DateTime.Today > DueDate.Date;
+        public TaskView(
+            int id,
+            int projectId,
+            string title,
+            string desc,
+            Priority priority,
+            DateTime dueDate,
+            bool isFinished)
+        {
+            Id = id;
+            ProjectId = projectId;
+            Title = title;
+            Description = desc;
+            Priority = priority;
+            DueDate = dueDate;
+            IsFinished = isFinished;
+        }
     }
 }
