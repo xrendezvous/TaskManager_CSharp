3108322bc952e9b0d6bf5f51c26aa2567e7e47ad
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2026-02-09 21:43:03 +0200

    added ProjectRecord

---
+++ TaskManager.Storage/Entities/ProjectRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Storage.Enums;
+
+
+namespace TaskManager.Storage.Entities
+{
+    public class ProjectRecord
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TypeOfProject Type { get; set; }
+
+        public ProjectRecord(int id, string name, string desc, TypeOfProject type) 
+        {
+            Id = id;
+            Name = name;
+            Description = desc;
+            Type = type;
+        }
+
+        public void UpdateRecord(string name, string desc, TypeOfProject type)
+        {
+            Name = name;
+            Description = desc;
+            Type = type;
+        }
+    }
+}
---
+++ TaskManager.Storage/Entities/TaskRecord.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Storage.Entities
+{
+    internal class TaskRecord
+    {
+    }
+}
