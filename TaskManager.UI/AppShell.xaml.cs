dd94fca697156261d63b56a54758ef6b6a582c8c
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2026-02-22 18:23:20 +0200

    add .NET MAUI App file

---
+++ TaskManager.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskManager.UI/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using TaskManager.Services;
+using TaskManager.UI;
+
+namespace TaskManager.AppUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder.UseMauiApp<App>();
+
+        builder.Services.AddSingleton<RepositoryService>();
+        /*
+        builder.Services.AddTransient<ProjectsPage>();
+        builder.Services.AddTransient<ProjectDetailsPage>();
+        builder.Services.AddTransient<TaskDetailsPage>();
+        */
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
